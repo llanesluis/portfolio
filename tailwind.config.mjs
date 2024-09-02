c34dfae8cee68087a10e570eed7a33a15c73a8db
Luis Llanes <luisllaboj@gmail.com>
2024-09-02 01:17:26 -0700

    add set-up config & project skeleton

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "rgb(var(--accent))",
+          light: "rgb(var(--accent-light))",
+          dark: "rgb(var(--accent-dark))",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
