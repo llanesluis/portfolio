c2a770357dd26dabcaa15dab1a07320c3bd400da
Luis Llanes <luisllaboj@gmail.com>
2024-10-15 16:18:41 -0700

    style: remove repeated section name and improved reusability in TagCodeComponent

--- src/components/TagCodeBlock.tsx
+++ src/components/TagCodeBlock.tsx
@@ -1,59 +1,68 @@
 import { useState, type ReactNode } from "react";
 import { cn } from "../lib/utils";
 
-interface SectionWrapperProps {
+interface TagCodeBlockProps {
   children: ReactNode;
   label: string;
   className?: string;
+  status?: "availableNow" | "working";
 }
 
-export default function TagCodeBlock({ children, label }: SectionWrapperProps) {
+export default function TagCodeBlock({
+  children,
+  label,
+  className,
+  status,
+}: TagCodeBlockProps) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div
       className={cn(
-        "relative isolate origin-top transition-all",
+        "relative isolate origin-top transition-all duration-75",
         "grid grid-rows-[auto_0fr_auto] content-start",
         isOpen && "grid-rows-[auto_1fr_auto]",
+        className,
       )}
     >
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className={cn(
-          "absolute -left-[22px] top-[6px]",
-          "transition duration-75",
-          isOpen ? "rotate-90" : "rotate-0",
-        )}
-      >
-        <span className="sr-only">Toggle open and close code block</span>
-        <Chevron className="text-muted-foreground/50 hover:text-muted-foreground" />
-      </button>
+      <div className={cn("relative", !isOpen && "bg-muted-foreground/10")}>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className={cn(
+            "absolute -left-[.9em] h-full",
+            "transition duration-75",
+            isOpen ? "rotate-90" : "rotate-0",
+          )}
+        >
+          <span className="sr-only">Toggle open and close code block</span>
+          <Chevron className="text-muted-foreground/50 hover:text-muted-foreground" />
+        </button>
 
-      <div className={cn(!isOpen && "bg-muted-foreground/10")}>
-        <TextMono text={"<"} className="text-3xl text-muted-foreground/70" />
-        <TextMono className="text-3xl font-semibold text-accent" text={label} />
-        <TextMono
-          text={`[data-status]="availableForWork"`}
-          className="ml-3 hidden text-3xl text-foreground/80 transition-all lg:inline-flex"
-        />
-        <TextMono text={">"} className="text-3xl text-muted-foreground/70" />
+        <TextMono text={"<"} className="text-muted-foreground/70" />
+        <TextMono className="font-semibold text-accent" text={label} />
+        {status && (
+          <TextMono
+            text={`[data-status]="${status}"`}
+            className="ml-3 hidden text-foreground/80 transition-all md:inline-flex"
+          />
+        )}
+        <TextMono text={">"} className="text-muted-foreground/70" />
       </div>
 
       <div
         className={cn(
-          "border-l-2 border-muted-foreground/50 pl-8",
+          "border-l-2 border-muted-foreground/50 pl-8 text-base",
           "overflow-hidden",
           "hover:border-muted-foreground",
         )}
       >
-        <div className="py-6">{children}</div>
+        <div className="py-4">{children}</div>
       </div>
 
       <div>
-        <TextMono text={"</"} className="text-3xl text-muted-foreground/70" />
-        <TextMono className="text-3xl font-semibold text-accent" text={label} />
-        <TextMono text={">"} className="text-3xl text-muted-foreground/70" />
+        <TextMono text={"</"} className="text-muted-foreground/70" />
+        <TextMono className="font-semibold text-accent" text={label} />
+        <TextMono text={">"} className="text-muted-foreground/70" />
       </div>
     </div>
   );
@@ -67,8 +76,8 @@ function Chevron({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="1em"
+      height="1em"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
