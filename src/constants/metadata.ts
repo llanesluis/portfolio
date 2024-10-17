6e9d65cafea57d90503f919822d6ea37ea105534
Luis Llanes <luisllaboj@gmail.com>
2024-10-17 02:36:11 -0700

    refactor: moved metadata to a constants file & added effect on hero

---
+++ src/constants/metadata.ts
@@ -0,0 +1,9 @@
+export const METADATA = {
+  title: "Luis Llanes | Software Engineer & full-stack developer",
+  description:
+    "Luis Llanes is a software engineer and full-stack developer. Mainly building web applications with React, Next.js, and TypeScript.",
+  url: "https://www.llanesluis.xyz/",
+  keywords:
+    "TypeScript, JavaScript, React, Next.js, Node.js, Express, FullStack, Frontend, Developer, Programmer, Programador, Desarrollador, Desarrollo web, Web, Páginas web, Aplicaciones web, Web apps, SEO, Astro, CSS, HTML",
+  ogImage: "/og_image.webp",
+};
