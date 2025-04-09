3e373a07b7596b0ba9a082fc43aae5665850cf67
Luis Llanes <luisllaboj@gmail.com>
2025-04-09 16:55:31 -0700

    chore: update projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,6 +1,8 @@
 import type { TechnologyLabel } from "./technologies";
 
-type Tags = Array<"side-project" | "learn" | "forgotten" | (string & {})>;
+type Tags = Array<
+  "side-project" | "learn" | "forgotten" | "WIP" | (string & {})
+>;
 
 export type Project = {
   title: string;
@@ -15,6 +17,17 @@ export type Project = {
 
 export const PROJECTS = [
   {
+    title: "themux",
+    description:
+      "A shadcn/ui theme generator, but fully customizable. Supports Tailwind v4 and v3 and different color formats.",
+    techStack: ["Nextjs", "TailwindCSS", "React", "TypeScript"],
+    imgUrl: "/projects/themux.webp",
+    sourceCodeUrl: "https://github.com/llanesluis/themux",
+    tags: ["side-project", "devtools", "ux/ui", "WIP"],
+    liveUrl: "https://themux.vercel.app/",
+    wip: true,
+  },
+  {
     title: "DevTool for building responsive websites",
     description:
       "Provides an easy way to track screen breakpoints and to switch between dark/light/system themes.",
@@ -23,42 +36,5 @@ export const PROJECTS = [
     sourceCodeUrl:
       "https://github.com/llanesluis/template/blob/main/src/components/devtools/screen-devtools.tsx",
     tags: ["devtools", "side-project", "learn"],
-  },
-  {
-    title: "Landing page for Las Lasañas LM",
-    description:
-      "Landing page for a small business specializing in lasagna as a fast-food alternative, designed to showcase their menu and social media presence.",
-    techStack: ["TypeScript", "Nextjs", "React", "TailwindCSS", "Git"],
-    imgUrl: "/projects/las_lasanas.webp",
-    liveUrl: "https://las-lasanas-lm.vercel.app/",
-    sourceCodeUrl: "https://github.com/llanesluis/las-lasanas-lm",
-    wip: true,
-    tags: ["side-project", "learn"],
-  },
-  {
-    title: "Image Editor SaaS",
-    description:
-      "Web app to enhance and transform images with AI. Was built to solve a problem I had working with low quality images for a family business.",
-    imgUrl: "/projects/image_editor_saas.webp",
-    techStack: [
-      "TypeScript",
-      "React",
-      "Nextjs",
-      "TailwindCSS",
-      "Clerk",
-      "MongoDB",
-    ],
-    sourceCodeUrl: "https://github.com/llanesluis/image-enhancer",
-    liveUrl: "https://image-enhancer-kappa.vercel.app/",
-    tags: ["learn", "forgotten", "side-project"],
-  },
-  {
-    title: "React Query V5 Demo",
-    description:
-      "This project was created to learn React Query and most of its features by building a simple CRUD app.",
-    imgUrl: "/projects/react_query_demo.webp",
-    techStack: ["React", "Vite", "React Query", "TailwindCSS"],
-    sourceCodeUrl: "https://github.com/llanesluis/tanstack-query-demo",
-    tags: ["learn"],
   },
 ] satisfies Project[];
